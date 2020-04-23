07b1681a4f254641bb13f9b592aa390dcec16c42
root <root@test.c.staging-214517.internal>
2020-04-23 15:23:37 -0600

    vo_ip v13

--- vo_ip/static/src/js/phone_call_details_extend.js
+++ vo_ip/static/src/js/phone_call_details_extend.js
@@ -4,8 +4,14 @@ odoo.define('voip.PhoneCallDetailsExtend', function (require) {
     var Phone = require('voip.PhoneCallDetails');
 
     Phone = Phone.extend({
-        
-        
+           /**
+         * @constructor
+         */
+        init: function (parent, phonecall) {
+            //this._super.apply(this, arguments);
+            this._super(...arguments);
+        },
+
     });
 
 
