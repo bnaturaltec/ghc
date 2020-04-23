ff05bee2e86da435c79b5d7be77462c9dd31c7ca
root <root@test.c.staging-214517.internal>
2020-04-23 16:07:03 -0600

    vo_ip v13

--- vo_ip/static/src/js/phone_call_details_extend.js
+++ vo_ip/static/src/js/phone_call_details_extend.js
@@ -1,24 +1,6 @@
 odoo.define('voip.PhoneCallDetailsExtend', function (require) {
     "use strict";
-    const core = require('web.core');
-    const session = require('web.session');
-    const Widget = require('web.Widget');
-
-    const QWeb = core.qweb;
-    const _t = core._t;
-
-    const Phone = require('voip.PhoneCallDetails');
-
-    Phone.PhoneCallDetails = Phone.PhoneCallDetails.extend({
-           /**
-         * @constructor
-         */
-        init: function (parent, phonecall) {
-            this._super.apply(this, arguments);
-            console.log("hola mundo");
-        },
-
-    });
+    
 
 
 });
