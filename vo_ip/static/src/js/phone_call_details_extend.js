7ac30773262d84366337bee36f79bc14d02030c3
root <root@test.c.staging-214517.internal>
2020-04-25 12:14:31 -0600

    :hammer:

--- vo_ip/static/src/js/phone_call_details_extend.js
+++ vo_ip/static/src/js/phone_call_details_extend.js
@@ -4,7 +4,22 @@ odoo.define('voip.PhoneCallDetailsExtend', function (require) {
     
     console.log("este es el widget",Phone)
 
-    console.log("phone.xxxxxx.xxxxxxxxx.xxxxxxx",Phone.extend())
+    console.log("phone.xxxxxx.xxxxxxxxx.xxxxxxx",Phone.extend({
+        events: _.extend({}, Phonecall.PhonecallDetails.events, {
+            'click .o_dial_activity_done': '_onClickActivityDone',
+            'click .o_dial_call_number': '_onClickCallNumber',
+            'click .o_dial_activity_cancel': '_onClickCancel',
+            'click .o_phonecall_details_close': '_onClickDetailsClose',
+            'click .o_dial_email': '_onClickEmail',
+            'click .o_dial_log': '_onClickLog',
+            'click .o_dial_mute_button': '_onClickMuteButton',
+            'click .o_dial_reschedule_activity': '_onClickRescheduleActivity',
+            'click .o_dial_to_partner': '_onClickToPartner',
+            'click .o_dial_to_record': '_onClickToRecord',
+            'click .o_dial_transfer_button': '_onClickTransferButton',
+            'click .o_dial_to_sale_order': '_onToSaleOrderClick',
+        }),
+    }))
 
     Phone = Phone.extend({
         
