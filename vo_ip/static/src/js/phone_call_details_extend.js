83b458864cb14b7ed101efbc89258488b6b6689c
root <root@test.c.staging-214517.internal>
2020-04-25 12:44:21 -0600

    :hammer:

--- vo_ip/static/src/js/phone_call_details_extend.js
+++ vo_ip/static/src/js/phone_call_details_extend.js
@@ -12,7 +12,7 @@ odoo.define('voip.PhoneCallDetailsExtend', function (require) {
   
     return Phone.extend({
         
-        prototype.events: _.extend({}, Phonecall.PhonecallDetails.events, {
+        events: _.extend({}, Phone.prototype.events, {
             'click .o_dial_activity_done': '_onClickActivityDone',
             'click .o_dial_call_number': '_onClickCallNumber',
             'click .o_dial_activity_cancel': '_onClickCancel',
