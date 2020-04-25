e456629db82b6d77d6733a5fb895edc14f31bcaf
root <root@test.c.staging-214517.internal>
2020-04-25 14:01:51 -0600

    :hammer:

--- vo_ip/static/src/js/phone_call_details_extend.js
+++ vo_ip/static/src/js/phone_call_details_extend.js
@@ -11,22 +11,6 @@ odoo.define('voip.PhoneCallDetailsExtend', function (require) {
     const _t = core._t;
   
     const PhoneCallDetails = Phone.extend({
-        
-        events: _.extend({}, Phone.prototype.events, {
-            'click .o_dial_activity_done': '_onClickActivityDone',
-            'click .o_dial_call_number': '_onClickCallNumber',
-            'click .o_dial_activity_cancel': '_onClickCancel',
-            'click .o_phonecall_details_close': '_onClickDetailsClose',
-            'click .o_dial_email': '_onClickEmail',
-            'click .o_dial_log': '_onClickLog',
-            'click .o_dial_mute_button': '_onClickMuteButton',
-            'click .o_dial_reschedule_activity': '_onClickRescheduleActivity',
-            'click .o_dial_to_partner': '_onClickToPartner',
-            'click .o_dial_to_record': '_onClickToRecord',
-            'click .o_dial_transfer_button': '_onClickTransferButton',
-            'click .o_dial_to_sale_order': '_onToSaleOrderClick',
-        }),
-
           /**
      * TODO: reduce coupling between PhoneCallDetails & PhoneCall
      *
@@ -46,13 +30,13 @@ odoo.define('voip.PhoneCallDetailsExtend', function (require) {
         console.log("start overrude");
     },
 
-    
+
 
     });
   
     
 
-
+console.log("override",PhoneCallDetails);
 return PhoneCallDetails;
 
 });
