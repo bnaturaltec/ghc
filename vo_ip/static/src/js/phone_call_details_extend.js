d282296e08d994c7f72e4255c011a798bb18d5f8
root <root@test.c.staging-214517.internal>
2020-04-25 15:00:58 -0600

    :hammer:

--- vo_ip/static/src/js/phone_call_details_extend.js
+++ vo_ip/static/src/js/phone_call_details_extend.js
@@ -38,36 +38,10 @@ odoo.define('voip.PhoneCallDetailsExtend', function (require) {
          * @param {MouseEvent} ev
          * @return {Promise}
          */
-        async _onToSaleOrderClick(ev) {
+        _onToSaleOrderClick: function (ev) {
             console.log("click");
             ev.preventDefault();
             let resId = this.partnerId;
-            if (!this.partnerId) {
-                let domain = [];
-                if (this.phoneNumber && this.mobileNumber) {
-                    domain = ['|',
-                        ['phone', '=', this.phoneNumber],
-                        ['mobile', '=', this.mobileNumber]];
-                } else if (this.phoneNumber) {
-                    domain = ['|',
-                        ['phone', '=', this.phoneNumber],
-                        ['mobile', '=', this.phoneNumber]];
-                } else if (this.mobileNumber) {
-                    domain = [['mobile', '=', this.mobileNumber]];
-                }
-                const ids = await this._rpc({
-                    method: 'search_read',
-                    model: "res.partner",
-                    kwargs: {
-                        domain,
-                        fields: ['id'],
-                        limit: 1,
-                    }
-                });
-                if (ids.length) {
-                    resId = ids[0].id;
-                }
-            }
             if (resId !== undefined) {
                 this.do_action({
                     res_id: resId,
@@ -75,17 +49,11 @@ odoo.define('voip.PhoneCallDetailsExtend', function (require) {
                     target: 'current',
                     type: 'ir.actions.act_window',
                     views: [[false, 'form']],
-                    /*context: {
-                        default_partner_id: ids[0], 
-                    }*/
                 });
             } else {
                 this.do_action({
                     context: {
-                        /*default_email: this.email,
-                        default_phone: this.phoneNumber,
-                        default_mobile: this.mobileNumber,
-                        */
+                        default_partner_id : resId,
                     },
                     res_model: 'sale.order',
                     target: 'current',
