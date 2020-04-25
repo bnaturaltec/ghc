e6e80a082ed6a1237fff52dcfcdb998ec8d2c2a8
root <root@test.c.staging-214517.internal>
2020-04-25 14:28:05 -0600

    :hammer:

--- vo_ip/static/src/js/phone_call_details_extend.js
+++ vo_ip/static/src/js/phone_call_details_extend.js
@@ -2,27 +2,25 @@ odoo.define('voip.PhoneCallDetailsExtend', function (require) {
     "use strict";
     var Phone = require('voip.PhoneCallDetails');
     
-    Phone.extend({
-          /**
-     * TODO: reduce coupling between PhoneCallDetails & PhoneCall
-     *
-     * @override
-     * @param {voip.PhoneCallTab} parent
-     * @param {voip.PhoneCall} phoneCall
-     */
-    init(parent, phoneCall) {
-        this._super(...arguments);
-        console.log("init overrude");
-    },
-    /**
-     * @override
-     */
-    start() {
-        this._super(...arguments);
-        console.log("start overrude");
-    },
-
-
+    Phone = Phone.extend({
+            /**
+         * TODO: reduce coupling between PhoneCallDetails & PhoneCall
+         *
+         * @override
+         * @param {voip.PhoneCallTab} parent
+         * @param {voip.PhoneCall} phoneCall
+         */
+        init(parent, phoneCall) {
+            this._super(...arguments);
+            console.log("init overrude");
+        },
+        /**
+         * @override
+         */
+        start() {
+            this._super(...arguments);
+            console.log("start overrude");
+        },
 
     });
   
