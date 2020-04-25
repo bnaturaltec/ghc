bc635b9d6aee1a1733bd7c782ccba40967865534
root <root@test.c.staging-214517.internal>
2020-04-25 13:58:05 -0600

    :hammer:

--- vo_ip/static/src/js/phone_call_details_extend.js
+++ vo_ip/static/src/js/phone_call_details_extend.js
@@ -27,68 +27,26 @@ odoo.define('voip.PhoneCallDetailsExtend', function (require) {
             'click .o_dial_to_sale_order': '_onToSaleOrderClick',
         }),
 
-                 /**
-         * @private
-         * @param {MouseEvent} ev
-         * @return {Promise}
-         */
-        async _onToSaleOrderClick(ev) {
-            ev.preventDefault();
-            console.log('press');
-            let resId = this.partnerId;
-            if (!this.partnerId) {
-                let domain = [];
-                if (this.phoneNumber && this.mobileNumber) {
-                    domain = ['|',
-                        ['phone', '=', this.phoneNumber],
-                        ['mobile', '=', this.mobileNumber]];
-                } else if (this.phoneNumber) {
-                    domain = ['|',
-                        ['phone', '=', this.phoneNumber],
-                        ['mobile', '=', this.phoneNumber]];
-                } else if (this.mobileNumber) {
-                    domain = [['mobile', '=', this.mobileNumber]];
-                }
-                const ids = await this._rpc({
-                    method: 'search_read',
-                    model: "res.partner",
-                    kwargs: {
-                        domain,
-                        fields: ['id'],
-                        limit: 1,
-                    }
-                });
-                if (ids.length) {
-                    resId = ids[0].id;
-                }
-            }
-            if (resId !== undefined) {
-                this.do_action({
-                    res_id: resId,
-                    res_model: "sale.order",
-                    target: 'current',
-                    type: 'ir.actions.act_window',
-                    views: [[false, 'form']],
-                    /*context: {
-                        default_partner_id: ids[0], 
-                    }*/
-                });
-            } else {
-                this.do_action({
-                    context: {
-                        /*default_email: this.email,
-                        default_phone: this.phoneNumber,
-                        default_mobile: this.mobileNumber,
-                        */
-                        default_partner_id: this.partnerId,
-                    },
-                    res_model: 'sale.order',
-                    target: 'current',
-                    type: 'ir.actions.act_window',
-                    views: [[false, 'form']],
-                });
-            }
-        },
+          /**
+     * TODO: reduce coupling between PhoneCallDetails & PhoneCall
+     *
+     * @override
+     * @param {voip.PhoneCallTab} parent
+     * @param {voip.PhoneCall} phoneCall
+     */
+    init(parent, phoneCall) {
+        this._super(...arguments);
+        console.log("init overrude");
+    },
+    /**
+     * @override
+     */
+    start() {
+        this._super(...arguments);
+        console.log("start overrude");
+    },
+
+    
 
     });
   
