452f99f1410663f2842e61180e8d18ac6c8d7f14
stefanjaw <stefanj@avalantec.com>
2020-05-30 13:56:54 -0600

    modified voip_iceserver manifest file

--- voip_iceserver/__manifest__.py
+++ voip_iceserver/__manifest__.py
@@ -3,8 +3,7 @@
     'name': "voip_custom",
 
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
+        voip_iceserver module that defines iceservers to [] to reduce delays""",
 
     'description': """
         Long description of module's purpose
