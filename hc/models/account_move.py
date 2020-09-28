688e2b071d87d8b4ae1cd22acc63f3d9b3be959b
stefanjaw <stefanj@avalantec.com>
2020-09-27 21:19:44 -0600

    Added to account_move tree view state canton distrito fields

--- hc/models/account_move.py
+++ hc/models/account_move.py
@@ -9,3 +9,7 @@ class AccountMove(models.Model):
     
     #warehouse_id = fields.Many2one(string='Almacen',related='stock.warehouse',store = True) 
     warehouse_id = fields.Many2one('stock.warehouse', string="Warehouse",store = True)
+
+    state_id = fields.Char(string='Provincia',related='partner_id.state_id.name', store=True)
+    canton_id = fields.Char(string='Canton',related='partner_id.canton_id.name', store=True)
+    distrito_id = fields.Char(string='Distrito',related='partner_id.distrito_id.name', store=True)
